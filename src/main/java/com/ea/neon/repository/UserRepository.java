c71034c1e8a7295b8efdb204b7265cf752ec60fc
Keshav Shahi <ksav.sai52@gmail.com>
2016-05-15 16:28:42 -0500

    updated UserService

--- src/main/java/com/ea/neon/repository/UserRepository.java
+++ src/main/java/com/ea/neon/repository/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ea.neon.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
+	
+	User findOneByEmail(String email);
 
 }
