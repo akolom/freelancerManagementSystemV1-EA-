5c4b55ef0f323560080f745b928e5a0c76106025
sabeen pradhan <sabeenpradhan@gmail.com>
2016-05-16 01:12:49 +0545

    "domain entities"

--- src/main/java/com/ea/neon/controller/HomeController.java
+++ src/main/java/com/ea/neon/controller/HomeController.java
@@ -1,13 +1,31 @@
 package com.ea.neon.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ea.neon.domain.User;
+import com.ea.neon.service.UserService;
+import com.ea.neon.service.impl.UserServiceImpl;
 
 @Controller
 public class HomeController {
 
+	
+	
 	@RequestMapping({ "/", "/index" })
 	public String home() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("context/applicationContext.xml");
+		UserService userService = (UserService) ctx.getBean("userServiceImpl");
+		User user = new User();
+		user.setFirstName("John");
+		user.setLastName("Doe");
+		user.setEmail("john@Doe.com");
+		
+
+		userService.save(user);
 		return "index";
 	}
 
